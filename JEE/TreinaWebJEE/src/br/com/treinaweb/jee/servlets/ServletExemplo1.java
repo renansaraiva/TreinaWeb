16380fcf1a548ae2d4e096531d28de8cb0815e8e
Renan Saraiva <renan.saraiva@gmail.com>
2016-04-11 14:00:21 -0300

    Criando servlets com as annotations da versão 3. 19%

--- JEE/TreinaWebJEE/src/br/com/treinaweb/jee/servlets/ServletExemplo1.java
+++ JEE/TreinaWebJEE/src/br/com/treinaweb/jee/servlets/ServletExemplo1.java
@@ -3,6 +3,7 @@ package br.com.treinaweb.jee.servlets;
 import java.io.IOException;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,18 +12,20 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ServletExemplo1
  */
-@WebServlet("/ServletExemplo1")
+@WebServlet(name = "ServletExemplo1", urlPatterns = { "/SvtEx1" }, initParams = {
+		@WebInitParam(name = "mensagem", value = "Bem-vindo ao teste de Servlets!") })
 public class ServletExemplo1 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
 	private String mensagem;
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ServletExemplo1() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ServletExemplo1() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see Servlet#init(ServletConfig)
@@ -33,9 +36,11 @@ public class ServletExemplo1 extends HttpServlet {
 	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		String nome = request.getParameter("nome");
 		response.getWriter().write("<html>");
@@ -44,7 +49,7 @@ public class ServletExemplo1 extends HttpServlet {
 		response.getWriter().write("    </head>");
 		response.getWriter().write("    <body>");
 		response.getWriter().write("        <h1>" + mensagem + "</h1>");
-		response.getWriter().write("        <p>Bem-vindo, "+ nome +"</p>");
+		response.getWriter().write("        <p>Bem-vindo, " + nome + "</p>");
 		response.getWriter().write("    </body>");
 		response.getWriter().write("</html>");
 	}
