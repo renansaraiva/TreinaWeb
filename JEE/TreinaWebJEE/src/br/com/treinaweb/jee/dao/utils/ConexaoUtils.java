ef59d7452a53ccdd36a770f76a1ee30e3525cc5c
Renan Saraiva <renan.saraiva@gmail.com>
2016-04-15 18:12:13 -0300

    Criando uma classe utilitária para gerenciar a conexão. 38%

---
+++ JEE/TreinaWebJEE/src/br/com/treinaweb/jee/dao/utils/ConexaoUtils.java
@@ -0,0 +1,23 @@
+package br.com.treinaweb.jee.dao.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoUtils {
+
+	public static Connection criarConexao()
+			throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.jdbc.Driver").newInstance();
+		String stringConexao = "jdbc:mysql://localhost:3606/tw_java_intermediario?user=root&password=root";
+		Connection conn = DriverManager.getConnection(stringConexao);
+
+		return conn;
+	}
+
+	public static void fecharConexao(Connection conn) throws SQLException {
+		if (conn != null) {
+			conn.close();
+		}
+	}
+}
